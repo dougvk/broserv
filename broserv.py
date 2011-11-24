595978aff2022f183e4d6979f62b7cf5e48211e7
Doug von Kohorn <dougvk@gmail.com>
2011-11-23 18:53:55 -0800

    Have torrents almost working, will comment code soon.

--- broserv.py
+++ broserv.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime as dt
 from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from werkzeug import secure_filename
 from subprocess import Popen, PIPE
@@ -7,6 +8,7 @@ UPLOAD_FOLDER = 'popcorn/'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['USE_X_SENDFILE'] = True
 
 @app.route('/')
 def hello_world():
@@ -16,14 +18,26 @@ def hello_world():
 def search():
     return render_template('search.html')
 
+@app.route('/torrent/<path:path>/', methods=['GET'])
+def seed_file(path):
+    true_path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], path)
+    directory = os.path.dirname(true_path)
+    timestamp = dt.now().strftime('%Y-%m-%d-')
+    torrent_path = "%s/%s%s.torrent" % (directory, timestamp, os.path.basename(true_path))
+    command = "ctorrent -t -u \"http://test.example.com\" -s %s %s" % (torrent_path, true_path)
+    filename = os.path.basename(torrent_path)
+    p = Popen(command, stdout=PIPE, shell=True)
+    p.communicate()
+    return send_from_directory(directory, filename, cache_timeout=1, as_attachment=True, mimetype='application/x-bittorrent')
+
 @app.route('/list/', methods=['GET'])
 def root_list():
     return list('')
 
-@app.route('/list/<path>/', methods=['GET'])
+@app.route('/list/<path:path>/', methods=['GET'])
 def list(path):
     final_path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], path)
-    files = Popen("ls -la %s" % final_path, stdout=PIPE, shell=True)
+    files = Popen("ls -la %s | grep -v \'.torrent$\'" % final_path, stdout=PIPE, shell=True)
     listing = create_ls_listing(files.stdout.read())
     return render_template('list_files.html', rel_path=request.path, listing=listing, path=path)
 
@@ -31,7 +45,7 @@ def list(path):
 def root_upload():
     return upload_file('')
 
-@app.route('/upload/<path>/', methods=['POST'])
+@app.route('/upload/<path:path>/', methods=['POST'])
 def upload_file(path):
     if request.method == 'POST':
         the_file = request.files['file']
@@ -49,13 +63,9 @@ def favicon():
             'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 def create_ls_listing(stdout):
-    print stdout
     newline_split = stdout.split('\n')[1:-1]
-    print newline_split
     listing = []
-    for (index, item) in enumerate(newline_split):
-        print item
-        if item[0] == 'd':
+    for item in newline_split:
             listing.append((item.split()[-1], 'directory'))
         if item[0] == '-':
             listing.append((item.split()[-1], 'file'))
